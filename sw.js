2996ca8a6302b9a662c6e69627343a845751913b
justafluggast <92870056+justafluggast@users.noreply.github.com>
2021-10-25 23:02:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,6 @@ const OFFLINE_URL = 'offline.html';
 
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Install');
-  
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     // Setting {cache: 'reload'} in the new request will ensure that the response
