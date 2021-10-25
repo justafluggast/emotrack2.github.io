3eb936be49734f5e5285623a528cb3f669918090
justafluggast <92870056+justafluggast@users.noreply.github.com>
2021-10-25 18:39:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const butInstall = document.getElementById('butInstall');
 
 /* Put code here */
 
+indow.addEventListener('beforeinstallprompt', (event) => {
+  console.log('👍', 'beforeinstallprompt', event);
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event;
+  // Remove the 'hidden' class from the install button container
+  divInstall.classList.toggle('hidden', false);
+});
 
 
 /* Only register a service worker if it's supported */
