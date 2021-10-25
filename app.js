9b25076c189db3c330f63ea1c2cbb72fc98fa728
justafluggast <92870056+justafluggast@users.noreply.github.com>
2021-10-25 23:04:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const divInstall = document.getElementById('installContainer');
+const butInstall = document.getElementById('butInstall');
+
 
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
