e1bf5c2bf6fb378a31fab171f3cf3b1dd4f418e1
justafluggast <92870056+justafluggast@users.noreply.github.com>
2021-10-25 14:08:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,5 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
-    // Registrierung erfolgreich
-    console.log('Registrierung erfolgreich. Scope ist ' + reg.scope);
-  }).catch(function(error) {
-    // Registrierung fehlgeschlagen
-    console.log('Registrierung fehlgeschlagen mit ' + error);
-  });
-};
+// Listen for install event, set callback
+self.addEventListener('install', function(event) {
+    // Perform some task
+});
+
