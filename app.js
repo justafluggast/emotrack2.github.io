428dbbf3a84bf9336b49463a0ec4b0771229d19a
justafluggast <92870056+justafluggast@users.noreply.github.com>
2021-10-25 23:01:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,3 @@
-const divInstall = document.getElementById('installContainer');
-const butInstall = document.getElementById('butInstall');
-
-/* Put code here */
-
-window.addEventListener('beforeinstallprompt', (event) => {
-  console.log('1', 'beforeinstallprompt', event);
-  // Stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-  // Remove the 'hidden' class from the install button container
-  divInstall.classList.toggle('hidden', false);
-});
-
-butInstall.addEventListener('click', async () => {
-  console.log('1', 'butInstall-clicked');
-  const promptEvent = window.deferredPrompt;
-  if (!promptEvent) {
-    // The deferred prompt isn't available.
-    return;
-  }
-  // Show the install prompt.
-  promptEvent.prompt();
-  // Log the result
-  const result = await promptEvent.userChoice;
-  console.log('1', 'userChoice', result);
-  // Reset the deferred prompt variable, since
-  // prompt() can only be called once.
-  window.deferredPrompt = null;
-  // Hide the install button.
-  divInstall.classList.toggle('hidden', true);
-});
 
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
