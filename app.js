89bbb74779591ebf35320664507e4f3b8c7a99c7
justafluggast <92870056+justafluggast@users.noreply.github.com>
2021-10-25 23:13:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const divInstall = document.getElementById('installContainer');
-const butInstall = document.getElementById('butInstall');
-
-window.addEventListener('beforeinstallprompt', (event) => {
-  console.log('👍', 'beforeinstallprompt', event);
-  // Stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-  // Remove the 'hidden' class from the install button container
-  divInstall.classList.toggle('hidden', false);
-});
 
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
